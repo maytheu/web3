a8281aadd9b5fa352f3004b1ac9aa01dfce98c0a
maytheu <maytheu98@gmail.com>
2025-01-02 18:03:36 +0100

    initial contract and rename birdz

---
+++ src/contracts/KryptoBirdz.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// contains all solidity code
+contract KryptoBirdz {
+    string public name;
+    string public symbol;
+
+    constructor() {
+        name = "Kryptobird";
+        symbol = "KBIRDZ";
+    }
+}
