52e95e15fc3c9e753c26be4303a53f787eda34fb
maytheu <maytheu98@gmail.com>
2025-01-06 23:01:31 +0100

    erc721 enumeration

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -16,25 +16,52 @@ contract ERC721Enumerable is ERC721 {
     /// @notice Count NFTs tracked by this contract
     /// @return A count of valid NFTs tracked by this contract, where each one of
     ///  them has an assigned and queryable owner not equal to the zero address
-    function totalSupply() external view returns (uint256) {
+    function totalSupply() public view returns (uint256) {
         return _allTokens.length;
     }
 
-   
+    /// @notice Enumerate valid NFTs
+    /// @dev Throws if `_index` >= `totalSupply()`.
+    /// @param _index A counter less than `totalSupply()`
+    /// @return The token identifier for the `_index`th NFT,
+    ///  (sort order not specified)
+    function tokenByIndex(uint256 _index) external view returns (uint256) {
+        require(_index <= totalSupply(), "Index greater than total supply");
+        return _allTokens[_index];
+    }
+
+    /// @notice Enumerate NFTs assigned to an owner
+    /// @dev Throws if `_index` >= `balanceOf(_owner)` or if
+    ///  `_owner` is the zero address, representing invalid NFTs.
+    /// @param _owner An address where we are interested in NFTs owned by them
+    /// @param _index A counter less than `balanceOf(_owner)`
+    /// @return The token identifier for the `_index`th NFT assigned to `_owner`,
+    ///   (sort order not specified)
+    function tokenOfOwnerByIndex(
+        address _owner,
+        uint256 _index
+    ) external view returns (uint256) {
+        require(_index <= balanceOf(_owner), "Index is greater than address balance");
+        require(_owner != address(0), "Invalid address");
+        return _ownedToken[_owner][_index];
+    }
 
     // function to inherit _mint() in ERC721
     // this overide the ERC721 def i.e virtual in ERC721
     function _mint(address to, uint tokenId) internal override(ERC721) {
         // run _mint in ERC721
         super._mint(to, tokenId);
-        _addTokenToTotalSupply(tokenId);
-    }
-
-    function _addTokenToTotalSupply(uint256 tokenId) private {
-        _allTokens.push(tokenId);
+        _addTokenToAllTokensEnumeration(tokenId);
+        _addTokenToOwnerEnumeration(to, tokenId);
     }
 
     function _addTokenToAllTokensEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
         _allTokens.push(tokenId);
+    }
+
+    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokenIndex[tokenId] = _ownedToken[to].length;
+        _ownedToken[to].push(tokenId);
     }
 }
