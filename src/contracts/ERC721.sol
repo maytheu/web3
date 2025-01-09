f436aca5524f30845448ccfc6192269eeeaccb46
maytheu <maytheu98@gmail.com>
2025-01-09 11:08:26 +0100

    interface for IERC721

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -1,40 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import './ERC165.sol';
+import "./ERC165.sol";
+import "./interfaces/IERC721.sol";
 
-contract ERC721 is ERC165 {
+contract ERC721 is ERC165, IERC721 {
     // map token id to address owner
     mapping(uint => address) private _tokenOwner;
     // map address to owned tokens
     mapping(address => uint) private _ownedTokens;
     mapping(uint256 => address) private _tokenApproval;
 
-    event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 indexed _tokenId
-    );
-
-    event Approval(
-        address indexed _owner,
-        address indexed _approved,
-        uint256 indexed _tokenId
-    );
+    // inherit from interfface
+    // event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    // event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId );
 
     // return total nfs owned/minted
-    function balanceOf(address _owner) public view returns (uint256) {
+    function balanceOf(address _owner) public view override returns (uint256) {
         require(_owner != address(0), "Address do not exist");
         return _ownedTokens[_owner];
     }
 
-    //return address based on mint index
-    /// @notice Find the owner of an NFT
-    /// @dev NFTs assigned to zero address are considered invalid, and queries
-    ///  about them do throw.
-    /// @param _tokenId The identifier for an NFT
-    /// @return The address of the owner of the NFT
-    function ownerOf(uint256 _tokenId) public view returns (address) {
+    function ownerOf(uint256 _tokenId) public view override returns (address) {
         require(_exist(_tokenId), "Token not found");
         return _tokenOwner[_tokenId];
         // address owner = _tokenOwner[_tokenId];
@@ -61,16 +48,6 @@ contract ERC721 is ERC165 {
         emit Transfer(address(0), to, tokenId);
     }
 
-    /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
-    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
-    ///  THEY MAY BE PERMANENTLY LOST
-    /// @dev Throws unless `msg.sender` is the current owner, an authorized
-    ///  operator, or the approved address for this NFT. Throws if `_from` is
-    ///  not the current owner. Throws if `_to` is the zero address. Throws if
-    ///  `_tokenId` is not a valid NFT.
-    /// @param _from The current owner of the NFT
-    /// @param _to The new owner
-    /// @param _tokenId The NFT to transfer
     function _transferFrom(
         address _from,
         address _to,
@@ -90,18 +67,16 @@ contract ERC721 is ERC165 {
         emit Transfer(_from, _to, _tokenId);
     }
 
-    function transferFrom(address _from, address _to, uint256 _tokenId) public {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    )  override public {
         require(_isApprovedOrOwner(msg.sender, _tokenId));
         _transferFrom(_from, _to, _tokenId);
     }
 
-    /// @notice Change or reaffirm the approved address for an NFT
-    /// @dev The zero address indicates there is no approved address.
-    ///  Throws unless `msg.sender` is the current NFT owner, or an authorized
-    ///  operator of the current owner.
-    /// @param _approved The new approved NFT controller
-    /// @param _tokenId The NFT to approve
-    function approve(address _approved, uint256 _tokenId) external {
+    function approve(address _approved, uint256 _tokenId) external override {
         address owner = ownerOf(_tokenId);
         require(_approved != owner, "Approval of current owner");
         require(msg.sender == owner, "You should be the onwer of this token");
@@ -121,11 +96,9 @@ contract ERC721 is ERC165 {
         return spender == owner || getApproved(tokenId) == spender;
     }
 
-    /// @notice Get the approved address for a single NFT
-    /// @dev Throws if `_tokenId` is not a valid NFT.
-    /// @param _tokenId The NFT to find the approved address for
-    /// @return The approved address for this NFT, or the zero address if there is none
-    function getApproved(uint256 _tokenId) public view returns (address) {
+    function getApproved(
+        uint256 _tokenId
+    ) public view override returns (address) {
         require(_exist(_tokenId), "Token not found");
         return _tokenApproval[_tokenId];
     }
